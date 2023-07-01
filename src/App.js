95193b3ce50aa279b5273e32765c730474f0b615
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-07-02 05:29:44 +0530

    Adding starting message

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import { Card, Icon } from 'semantic-ui-react';
 
 function App() {
     return (
         <div className="App">
             <NavBar />
+            <Card centered>
+                <Card.Content>
+                    <Card.Header>Under Construction</Card.Header>
+                    <Card.Meta>
+                        <span className="date">Started 07/2023</span>
+                    </Card.Meta>
+                    <Card.Description>
+                        This website is under construction
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <a>
+                        <Icon name="computer" />
+                        Work in progress ! Stay Clear !
+                    </a>
+                </Card.Content>
+            </Card>
             <Footer />
         </div>
     );
