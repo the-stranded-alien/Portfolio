b2ee30597ba11061d0258688b26c5fdfe65bebec
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-07-03 00:29:19 +0530

    Removing unused vars

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Dropdown, Icon, Image } from 'semantic-ui-react';
+import { Menu, Icon } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import './NavBar.css';
 
--- src/components/Skills/SkillItem.js
+++ src/components/Skills/SkillItem.js
@@ -1,1 +1,5 @@
-const SkillItem = () => {};
+import { Slider } from 'semantic-ui-react';
+
+const SkillItem = (min, max, value) => {
+    return <Slider min={min} max={max} value={value} />;
+};
