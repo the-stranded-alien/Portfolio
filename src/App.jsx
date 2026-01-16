48180b34e6695a49c2ae82c08dce0a5c13398eb1
Sahil Gupta <sahil16gupta11@gmail.com>
2026-01-16 22:54:45 +0530

    Updating directory

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <ThemeProvider>
-      <Router basename="/Portfolio">
+      <Router>
         <div className="min-h-screen flex flex-col relative">
           <CosmicBackground />
           <Navbar personalData={data.personal} />
